84307f296532bf02a62c58f8cac074f2467e6f34
mOszz <victor.wacquet07@gmail.com>
2023-10-15 04:43:56 +0200

    Implement read & write methods to manipulate files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ use manager::{Operations, FileManager};
 
 fn main() {
     let file_manager = FileManager;
-    file_manager.list_files("./");
+
+    file_manager.read_file("./src/main.rs").expect("TODO: panic message");
+
+    //file_manager.write_file();
+
+    //file_manager.list_files("./");
+
     // match file_manager.create_file("file.txt") {
     //     Ok(()) => println!("File created successfully."),
     //     Err(e) => panic!("File creation failed: {:?}", e),
--- src/manager.rs
+++ src/manager.rs
@@ -1,9 +1,13 @@
-use std::fs::{create_dir, DirEntry, File, read_dir};
+use std::fs;
+use std::fs::{create_dir, File, read_dir, read_to_string};
+use std::io::Write;
 
 pub trait Operations {
     fn create_file(&self, file_name: &str) -> std::io::Result<()>;
     fn create_directory(&self, dir_name: &str) -> std::io::Result<()>;
     fn list_files(&self, read_dir: &str) -> std::io::Result<()>;
+    fn read_file(&self, file_path: &str) -> std::io::Result<()>;
+    fn write_file(&self, file_name: &str, file_content: &str) -> std::io::Result<()>;
 }
 
 pub struct FileManager;
@@ -24,6 +28,18 @@ impl Operations for FileManager {
         for file in read_dir(dir).unwrap() {
             println!("{}", file.unwrap().path().display());
         }
+        Ok(())
+    }
+
+    fn read_file(&self, file_path: &str) -> std::io::Result<()> {
+        let contents = read_to_string(file_path)
+            .expect("Didn't able to read the file");
+        println!("{}", contents);
+        Ok(())
+    }
+
+    fn write_file(&self, file_name: &str, file_content: &str) -> std::io::Result<()> {
+        fs::write(file_name, file_content)?;
         Ok(())
     }
 }
@@ -48,5 +64,17 @@ mod tests {
     fn test_list_files() {
         let file_manager = FileManager;
         assert!(file_manager.list_files("./").is_ok());
+    }
+
+    #[test]
+    fn test_read_file() {
+        let file_manager = FileManager;
+        assert!(file_manager.read_file("./test_file.txt").is_ok());
+    }
+
+    #[test]
+    fn test_write_file() {
+        let file_manager = FileManager;
+        assert!(file_manager.write_file("test_file.txt", "test content").is_ok());
     }
 }
