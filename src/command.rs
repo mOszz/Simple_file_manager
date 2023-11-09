e8fb5d70ff82fdcd237dcefab060076fe898f1f9
mOszz <victor.wacquet07@gmail.com>
2023-11-09 20:58:59 +0100

    Add args to commands.Complete list command.

--- src/command.rs
+++ src/command.rs
@@ -1,20 +1,82 @@
 use std::error::Error;
+use std::io;
+use crate::manager::{FileManager, Operations};
+
 pub struct Command {
     pub name: String,
     pub command: String,
-    pub args: Vec<String>,
+    pub args: (Vec<String>, bool),
     pub description: String,
     pub action: fn(Vec<String>) -> Result<(), Box<dyn Error>>,
 }
 
 impl Command {
-    pub fn new(name: String, command: String, args: Vec<String>, description: String, action: fn(Vec<String>) -> Result<(), Box<dyn Error>>) -> Self {
+    pub fn new(name: String, command: String, args: (Vec<String>, bool), description: String, action: fn(Vec<String>) -> Result<(), Box<dyn Error>>) -> Self {
         Command {
             name,
             command,
             args,
             description,
             action,
+        }
+    }
+
+    pub fn read_user_input() -> String {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read in command");
+
+        input.trim().to_string()
+    }
+
+    pub fn parse_user_input(input: &str) -> Vec<String> {
+        input.trim().split_whitespace().map(String::from).collect()
+    }
+
+    pub fn identify_args(words: Vec<String>) -> (String, Vec<String>) {
+        if words.is_empty() {
+            return (String::new(), Vec::new());
+        }
+        let command = words[0].clone();
+        let args = words[1..].to_vec();
+        (command, args)
+    }
+
+    pub fn execute_command(command: &str, args: Vec<String>, commands: &Vec<Command>) {
+        if let Some(cmd) = commands.iter().find(|cmd| cmd.command == command) {
+            let (expected_args, need_args) = &cmd.args;
+            if !need_args {
+                if args.len() != expected_args.len() {
+                    eprintln!("Usage: {} {}", cmd.command, expected_args.join(" "));
+                    return;
+                }
+            }
+
+            match (cmd.action)(args) {
+                Ok(_) => {
+                    println!("Command executed successfully");
+                }
+                Err(err) => {
+                    eprintln!("Command failed: {}", err);
+                }
+            }
+        } else {
+            println!("Command not found");
+        }
+    }
+
+    pub fn list_files_action(args: Vec<String>) -> Result<(), Box<dyn Error>> {
+        let file_manager = FileManager::new();
+
+        let main_args = if args.len() > 0 {
+            args[0].clone()
+        } else {
+            "./".to_string()
+        };
+
+        match file_manager.list_files(&main_args) {
+            Ok(_) => Ok(()),
+            Err(err) => Err(Box::new(err)),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,14 @@ pub mod manager;
 pub mod command;
 
 use std::error::Error;
-use std::io;
 use std::io::{stdout, Write};
 use manager::{Operations, FileManager};
 use command::Command;
 
 fn app_running() {
     let prompt_char = '🦀';
-
     print!("{} ", prompt_char);
     stdout().flush().unwrap();
-}
-
-fn read_user_input() -> String {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read in command");
-
-    return input.trim().to_string();
-}
-
-fn parse_user_input(input: &str) -> Vec<String> {
-    input.trim().split_whitespace().map(String::from).collect()
-}
-
-fn identify_args(words: Vec<String>) -> (String, Vec<String>) {
-    if words.is_empty() {
-        return (String::new(), Vec::new());
-    }
-    let command = words[0].clone();
-    let args = words[1..].to_vec();
-    (command, args)
-}
-
-fn execute_command(command: &str, args: Vec<String>, commands: &Vec<Command>) {
-    if let Some(cmd) = commands.iter().find(|cmd| cmd.command == command) {
-        match (cmd.action)(args) {
-            Ok(_) => {
-                println!("Command executed successfully");
-            }
-            Err(err) => {
-                eprintln!("Command failed: {}", err);
-            }
-        }
-    } else {
-        println!("Command not found");
-    }
-}
-
-fn list_files_action(args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    let file_manager = FileManager::new();
-    match file_manager.list_files("/.") {
-        Ok(_) => Ok(()),
-        Err(err) => Err(Box::new(err)),
-    }
 }
 
 fn main() {
@@ -63,33 +17,31 @@ fn main() {
         Command {
             name: "List".to_string(),
             command: "l".to_string(),
-            args: vec![String::from("./")],
+            args: (vec![String::from("<directory>")], true),
             description: "Command to list all directory or specific one".to_string(),
-            action: list_files_action,
+            action: Command::list_files_action,
         },
-
-
     ];
 
     loop {
         app_running();
 
-        let user_input = read_user_input();
-        let parsed_input = parse_user_input(&user_input);
-        let (command, args) = identify_args(parsed_input);
+        let user_input = Command::read_user_input();
+        let parsed_input = Command::parse_user_input(&user_input);
+        let (command, args) = Command::identify_args(parsed_input);
 
-        execute_command(&command, args, &commands);
+        Command::execute_command(&command, args, &commands);
     }
-
+}
     //file_manager.list_files("src").expect("TODO: panic message");
     //file_manager.create_file("test.txt");
-    //file_manager.create_file("C:/Users/victo/Documents/test.txt");
-    //file_manager.read_file("C:/Users/victo/Documents/dev/rust/simple_file_manager/src/main.rs");
-    //file_manager.write_file("C:/Users/victo/Documents/dev/rust/simple_file_manager/test.txt", "udidhbdi");
+    //file_manager.create_file("C:/Users/victor/Documents/test.txt");
+    //file_manager.read_file("C:/Users/victor/Documents/dev/rust/simple_file_manager/src/main.rs");
+    //file_manager.write_file("C:/Users/victor/Documents/dev/rust/simple_file_manager/test.txt", "udidhbdi");
     //file_manager.copy_file("test.txt", "src/test.txt");
     //file_manager.rename_or_move_file("dddd", "dddd");
     //file_manager.delete_file("sdf");
     //file_manager.create_directory("./src");
     //file_manager.list_files("/toto/failed test");
     //file_manager.delete_file("./test/delete/folder");
-}
+
